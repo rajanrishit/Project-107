701a6a6e64f93893b5eac88366efe2ea50bb1811
rajanrishit <87656072+rajanrishit@users.noreply.github.com>
2022-12-02 18:52:39 +0530

    Add files via upload

---
+++ object_tracking.py
@@ -0,0 +1,51 @@
+import cv2
+import time
+import math
+
+p1 = 530
+p2 = 300
+
+xs = []
+ys = []
+
+video = cv2.VideoCapture("bb3.mp4")
+
+# Load tracker 
+tracker = cv2.TrackerCSRT_create()
+
+# Read the first frame of the video
+returned, img = video.read()
+
+# Select the bounding box on the image
+bbox = cv2.selectROI("Tracking", img, False)
+
+# Initialise the tracker on the img and the bounding box
+tracker.init(img, bbox)
+
+print(bbox)
+
+def drawbox(img,bbox):
+    x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
+
+    cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
+
+    cv2.putText(img,"Tracking",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
+
+while True:
+    check,img = video.read()
+    success,bbox = tracker.update(img)
+
+    if success:
+        drawBox(img, bbox)
+    else:
+        cv2.putText(img,"Lost",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+
+    key = cv2.waitKey(25)
+    if key == ord('q'):
+        print("Closing")
+        break
+
+video.release()
+cv2.destroyALLwindows()   
+
+
